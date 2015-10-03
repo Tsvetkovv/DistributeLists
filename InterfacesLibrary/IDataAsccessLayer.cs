35a882a16149135dc40be60b01a367ceccd48b9d
Цветков Виктор Алексеевич <zwetvik@gmail.com>
2015-10-03 16:37:36 +0300

    add files

---
+++ InterfacesLibrary/IDataAsccessLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace InterfacesLibrary
+{
+    public interface IDataAsccessLayer
+    {
+        void AddUser(User addingUser);
+        void AddUserToDistributeList(User addingUser, DistributeList distributeList);
+        List<DistributeList> GetDistributeLists();
+        List<User> GetUsers();
+        void SaveDistributeLists(List<DistributeList> savingDistributeLists);
+        void SaveUserList(List<User> savingUsers);
+        void SaveAllFromCache();
+    }
+}
